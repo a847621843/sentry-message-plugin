9d6e9d865da833b700518a4bc772f2d60276eb7f
黄振 <847621843@qq.com>
2019-09-03 10:29:10 +0800

    修改版本号

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="sentry-message-plugin",
-    version='1.5',
+    version='1.6',
     author='zhen.huang',
     author_email='huangzhen@xinchanedu.com',
     url='https://github.com/a847621843/sentry-message-plugin',
